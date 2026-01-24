5c1c6a4ef4961b23f51f11d3a10bb11470d4f23b
bahaKz22 <talgatulybagdaulet08@gmail.com>
2026-01-24 15:43:32 +0500

    practices

---
+++ Practices/Strings/str.py
@@ -0,0 +1,10 @@
+a = "Hello"
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+c = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a,b,c)
